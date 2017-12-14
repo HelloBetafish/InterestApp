76d0dfbeb1f08db97a5282f19ae0807f8c2e277d
Your Name Here <tdsteph1@asu.edu>
2017-12-14 13:20:00 -0700

    Background for login inside public/index.html

--- client/src/components/Login/Navbar/Navbar.js
+++ client/src/components/Login/Navbar/Navbar.js
@@ -1,11 +1,25 @@
 import React from "react";
+import "./Navbar.css";
 
 const Navbar = props =>
 
-<nav class="navbar navbar-light bg-light">
+<nav class="navbar navbar-light">
   <a class="navbar-brand" href="#">
-    Bootstrap
+    <img src="images/Idea.jpg" class="img-fluid" alt="Responsive image">
+
+    </img>
+
+    <h2> Interest App </h2>
   </a>
+
+  <a className="navbar-brand" >
+      
+  </a>
+
+  <a className="navbar-brand" >
+    
+  </a>
+
 </nav>
 
 export default Navbar;
