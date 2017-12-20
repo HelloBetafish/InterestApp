357b643b1730dbb9ea0878e8b546a318ad645db8
YoSoyJesseFuerte <fortejesse@yahoo.com>
2017-12-20 13:07:33 -0700

    seeds sql info

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,6 +1,26 @@
-INSERT INTO app (name) VALUES ('');
+INSERT INTO app (FirstName,LastName) VALUES ('','');
 INSERT INTO app (date) VALUES ('');
-INSERT INTO app (gender) VALUES ('', true);
-INSERT INTO app (occupation) VALUES ('', true);
-INSERT INTO app (occupation) VALUES ('', true);
+INSERT INTO app (gender) VALUES ('');
+INSERT INTO app (occupation) VALUES ('');
 INSERT INTO app (interest) VALUES ('');
+
+INSERT INTO app (techInterest) VALUES ('','');
+INSERT INTO app (techExperience) VALUES ('','');
+INSERT INTO app (techYears) VALUES ('','');
+INSERT INTO app (artInterest) VALUES ('','');
+INSERT INTO app (artExperience) VALUES ('','');
+INSERT INTO app (artYears) VALUES ('','');
+INSERT INTO app (scienceInterest) VALUES ('','');
+INSERT INTO app (scienceExperience) VALUES ('','');
+INSERT INTO app (scienceYears) VALUES ('','');
+INSERT INTO app (networkInterest) VALUES ('','');
+INSERT INTO app (networkExperience) VALUES ('','');
+INSERT INTO app (networkYears) VALUES ('','');
+INSERT INTO app (entertainmentInterest) VALUES ('','');
+INSERT INTO app (entertainmentExperience) VALUES ('','');
+INSERT INTO app (entertainmentYears) VALUES ('','');
+INSERT INTO app (entrepreneurInterest) VALUES ('','');
+INSERT INTO app (entrepreneurExperience) VALUES ('','');
+INSERT INTO app	(entrepreneurYears) VALUES ('','');
+
+ 
