403e49bbdfba14a8c180e3e12b9e6ff5ca1abf40
YoSoyJesseFuerte <fortejesse@yahoo.com>
2017-12-20 12:27:00 -0700

    New SQL Database

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO app (name) VALUES ('');
+INSERT INTO app (date) VALUES ('');
+INSERT INTO app (gender) VALUES ('', true);
+INSERT INTO app (occupation) VALUES ('', true);
+INSERT INTO app (occupation) VALUES ('', true);
+INSERT INTO app (interest) VALUES ('');
