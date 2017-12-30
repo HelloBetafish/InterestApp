d80ccf4b05a38720fade2796daad326bdcd24b14
Your Name Here <tdsteph1@asu.edu>
2017-12-29 17:51:25 -0700

    add more to login

--- client/src/components/Login/FormList/index.js
+++ client/src/components/Login/FormList/index.js
@@ -1,2 +1,3 @@
 export * from "./FormList";
 export * from "./Textfield";
+export * from "./Select";
--- server.js
+++ server.js
@@ -41,9 +41,9 @@ app.use(express.static("client/build"));
 
 // Send every request to the React app
 // Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+//app.get("*", function(req, res) {
+ // res.sendFile(path.join(__dirname, "./client/build/index.html"));
+//});
 
 app.listen(PORT, function() 
 {
