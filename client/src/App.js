9fb34d1eefc24d3ebedecd7912aacb93e33fbb94
Your Name Here <tdsteph1@asu.edu>
2018-01-04 15:17:08 -0700

    added photo url and updated seedDB & models/users.js

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Profile from "./pages/Profile";
 import ConnectColl from "./pages/ConnectColl";
 import Messages from "./pages/Messages";
-//import Navbar from "./components/Navbar";
-
 import AddIdea from "./pages/AddIdea";
 import CreateAcct from "./pages/CreateAcct";
-
- import Login from "./pages/Login";
-
+import Login from "./pages/Login";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
--- models/user.js
+++ models/user.js
@@ -58,6 +58,12 @@ const userSchema = new Schema({
     type: Boolean,
     default: false,
   
+  },
+
+  photoURL:
+  {
+    type: String,
+    default: ""
   }
 
 
