75fdd44bc94a5736d1a35882c54d54955b8647d7
Unknown <reynamillan@hotmail.com>
2018-01-04 18:20:22 -0700

    Distance between btns and footer win the connectcoll page

--- client/src/pages/ConnectColl.js
+++ client/src/pages/ConnectColl.js
@@ -160,7 +160,7 @@ class ConnectColl extends Component {
     <Col size="md-3" className="zoom">
 
 
-                <div className="img-thumbnail">
+                <div className="img-thumbnail" style={{marginTop:"-5px"}}>
                       <img src="css/images/reyna2.jpg" width="150" height="150" style={{marginLeft:"45px"}}/>
                     <div className="people">
                         <p id="texta">Reyna</p> 
@@ -175,7 +175,7 @@ class ConnectColl extends Component {
             
 
              
-                <div className="img-thumbnail">
+                <div className="img-thumbnail" style={{marginTop:"-5px"}}>
                       <img src="css/images/Emma.jpg" width="150" height="150" style={{marginLeft:"45px"}}/>
                     <div className="people">
                         <p id="texta">Emma</p> 
@@ -191,7 +191,7 @@ class ConnectColl extends Component {
             
 
              
-                <div className="img-thumbnail">
+                <div className="img-thumbnail" style={{marginTop:"-5px"}}>
                       <img src="css/images/tim.jpg" width="150" height="150" style={{marginLeft:"45px"}}/>
                     <div className="people">
                         <p id="texta">Tim</p> 
@@ -208,7 +208,7 @@ class ConnectColl extends Component {
             
 
              
-                <div className="img-thumbnail">
+                <div className="img-thumbnail" style={{marginTop:"-5px"}}>
                       <img src="css/images/zac.jpg" width="150" height="150" style={{marginLeft:"45px"}}/>
                     <div className="people">
                         <p id="texta">Zac</p> 
