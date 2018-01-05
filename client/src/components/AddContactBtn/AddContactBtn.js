a51c405dbfabafc6de2cd53e1cdf7927a8e3d945
Unknown <reynamillan@hotmail.com>
2018-01-04 19:39:39 -0700

    I added post button in the  addidea page

--- client/src/components/AddContactBtn/AddContactBtn.js
+++ client/src/components/AddContactBtn/AddContactBtn.js
@@ -3,11 +3,7 @@ import PropTypes from 'prop-types'
 import "./AddContactBtn.css";
 
 const AddContactBtn = props => (
-<<<<<<< HEAD
-<button id="botones" style={{marginTop:"-20px", marginLeft:"50px",marginBottom: "50px"}} type="button" className="btn btn-outline-danger" onClick={props.handleClick}>Add to Contacts</button>
-=======
-<button style={{marginTop:"-20px", marginLeft:"50px"}} type="button" className="btn btn-outline-danger" data-id={props.id} onClick={props.handleClick}>Add to Contacts</button>
->>>>>>> master
+<button id="botones" style={{marginTop:"-20px", marginLeft:"50px", marginBottom:"50px"}} type="button" className="btn btn-outline-danger" data-id={props.id} onClick={props.handleClick}>Add to Contacts</button>
 );
 
 AddContactBtn.propTypes = {
