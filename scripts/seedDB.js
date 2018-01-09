bcd8726d21dc2c93b26f37b6070b5471c82f28fd
Your Name Here <tdsteph1@asu.edu>
2018-01-08 21:04:11 -0700

    more users added

--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -62,7 +62,33 @@ mongoose.Promise = global.Promise;
  	experience: "5 years",
  	online: false,
  	photoURL: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAIABADGAAAAAQAAAAAAAA02AAAAJDhkOGZiM2E5LTQxMjYtNDc0YS1hMzI1LTNiMmFiZTI1ZWRkMQ.jpg"
- }
+ },
+
+ {
+  username: "Bill505",
+  password: "U2FsdGVkX1+jqZJflJwT8lb+PykvPm3dWFIOVF6bCSU=",
+  title: "Lawyer",
+  full_name: "Bill Johnson",
+  email: "Bill@gmail.com",
+  country: "USA",
+  skills: "Law, Contracts",
+  experience: "10 years",
+  online: false,
+  photoURL: "http://thispix.com/wp-content/uploads/2015/06/portrait-profile-007.jpg"
+ },
+
+ {
+  username: "Jen605",
+  password: "U2FsdGVkX1+jqZJflJwT8lb+PykvPm3dWFIOVF6bCSU=",
+  title: "Lawyer",
+  full_name: "Jennifer Rodriguez",
+  email: "Jen@gmail.com",
+  country: "USA",
+  skills: "Law, Contracts",
+  experience: "15 years",
+  online: false,
+  photoURL: "https://i.pinimg.com/736x/aa/3f/f6/aa3ff6d2f13d4b8ae4aec358a51204a4--business-headshots-business-portrait.jpg"
+ },
 
 
 
