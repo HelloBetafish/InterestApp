5aaa7309320c79989f53f9e0208db0afdd7343b7
HelloBetafish <li_knee@yahoo.com>
2018-01-09 12:26:24 -0700

    Converted MeetUs.html to MeetUs.js page, but carousel not working

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Messages from "./pages/Messages";
 import PublicProfile from "./pages/PublicProfile";
 import CreateAcct from "./pages/CreateAcct";
 import Login from "./pages/Login";
+import MeetUs from "./pages/MeetUs";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 
@@ -24,6 +25,7 @@ const App = () =>
         <Route exact path="/connect&collaborate" component={ConnectColl} />
         <Route exact path="/publicprofile" component={PublicProfile} />
         <Route exact path="/createaccount" component={CreateAcct} />
+        <Route exact path="/meetus" component={MeetUs} />
       </Wrapper>
       <Footer />
     </div>
