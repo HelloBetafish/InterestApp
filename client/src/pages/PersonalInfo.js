75a520fc7a9371afe8de2ca417968db720354eb8
Unknown <reynamillan@hotmail.com>
2018-01-08 20:15:48 -0700

    added images in images folder

--- client/src/pages/PersonalInfo.js
+++ client/src/pages/PersonalInfo.js
@@ -195,40 +195,34 @@ const AddIdea = () =>(
                <hr/>      
 
               </div>
+         
+                   
+                                 
+          </div>
+
+
+
+ 
+        
+               
+
+  
+
+         
+        
+
             
-       
-          
-    
-
-   
-
-
-
-
-
-
-
-
-
-             <div className="container">
-    <div className="row">
-          
-     
-
-
-                  
                 
-                    <div className="col-md-4">
-                        
-                         
-                         
-                   </div>
-              
+          
 
-</div>
 
-</div>
-</div>
+                
+        
+
+
+
+
+  
 
 )
 
