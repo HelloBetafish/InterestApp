c150dd0b0abcd845382bddd5d6ce3e557a29d204
Your Name Here <tdsteph1@asu.edu>
2018-01-12 15:47:14 -0700

    fix errors

--- models/index.js
+++ models/index.js
@@ -4,13 +4,8 @@ module.exports =
   Signin: require("./signin"),
   Idea: require("./idea"),
   File: require("./File"),
-<<<<<<< HEAD
   Contact: require("./Contact"),
   Post: require("./Post"),
   Profile: require("./profile")
-=======
-  // Contact: require("./Contact"),
-  Post: require("./Post")
->>>>>>> bc476aa325275fee3b231eef6e496490963a3a17
-  
+ 
 };
