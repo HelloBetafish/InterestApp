f04e332b0f516f096d68d0120bf587e2f0ae1a07
Unknown <reynamillan@hotmail.com>
2018-01-13 01:50:11 -0700

    front end about.js page

--- client/src/pages/Aboutus.js
+++ client/src/pages/Aboutus.js
@@ -7,8 +7,65 @@ const AboutUs = () =>
 <div>
 <Navbar />
 
-<div className="cards" style={{width:"20px",height:"20px", background:"gray"}}></div>
-<p>ndljkflkfjg</p>
+
+
+<div className="cards">
+  <div className="imgBox"><img src="css/images/people4.jpg" style={{width:"100%",height:"100%"}}/></div>
+    <div className="details">
+      <div className="content">
+
+       <h3 style={{color:"black"}}>You are a one step <br/><spna>to make it real!</spna></h3>
+
+      </div>
+    </div>  
+</div>
+
+
+<div className="boxy" col-md-12>
+ 
+ <div className="row">
+
+
+
+
+
+	 <div className="flip">
+	   <div className="flip connect" >
+	      <div className="front"  ><p >CONNECT</p></div>
+	      <div className="back"><p>This is a meeting space to find like-minded creatives. Meet people like you!.</p></div>
+	   </div>
+	 </div>
+
+	 <div className="flip">
+	   <div className="flip collaborate">
+	     <div className="front"><p >COLLABORATE</p></div>
+	     <div className="back"><p>Pull together your knowledge with their experiences.</p></div>
+	   </div>
+	 </div>
+
+
+	 <div className="flip">
+	   <div className="flip innovate">
+	     <div className="front"><p>INNOVATE</p></div>
+	     <div className="back"><p> Share your ideas and see how it resounds with the community.</p></div>
+	    </div>
+	   </div>
+   </div>
+ </div>
+
+
+
+ 
+
+
+
+ </div>
+
+
+
+
+
+		
 
 
 
@@ -18,8 +75,5 @@ const AboutUs = () =>
 
 
 
-
-
- </div>;
 
 export default AboutUs; 
