ee75fda33b140d73ee069ebd527fa57b47de809d
HelloBetafish <li_knee@yahoo.com>
2018-01-12 17:24:31 -0700

    deleted Contact.js from models and removed it as a requirement from index.js

--- models/index.js
+++ models/index.js
@@ -4,7 +4,6 @@ module.exports =
   Signin: require("./signin"),
   Idea: require("./idea"),
   File: require("./File"),
-  Contact: require("./Contact"),
   Post: require("./Post"),
   Profile: require("./profile")
  
